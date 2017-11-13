295cd3cba34d90c5f8f38198f9831add8403dcc7
Tanner Gooding <tagoo@outlook.com>
2017-11-13 13:47:20 -0800

    Updating netci.groovy to support PR and Debug jobs

--- netci.groovy
+++ netci.groovy
@@ -8,57 +8,45 @@ import jobs.generation.Utilities;
 
 def project = GithubProject
 def branch = GithubBranchName
-def isPR = true
-
-def osList = ['OSX10.12', 'Windows_NT']
-def architectureList = ['x64']
-def configList = ['Release']
 
 def static getBuildJobName(def configuration, def os, def architecture) {
     return configuration.toLowerCase() + '_' + os.toLowerCase() + '_' + architecture.toLowerCase()
 }
 
-osList.each { os ->
-    architectureList.each { architecture ->
-        configList.each { config ->
-            // Calculate job name
-            def jobName = getBuildJobName(config, os, architecture)
-            def buildCommand = '';
+['OSX10.12', 'Ubuntu16.04', 'Windows_NT'].each { os ->
+    ['x64'].each { architecture ->
+        ['Debug', 'Release'].each { config ->
+            [true, false].each { isPR
+                // Calculate job name
+                def jobName = getBuildJobName(config, os, architecture)
+                def buildCommand = '';
 
-            def osBase = os
-            def machineAffinity = 'latest-or-auto'
+                def osBase = os
+                def machineAffinity = 'latest-or-auto'
 
-            // Calculate the build command
-            if (os == 'Windows_NT') {
-                buildCommand = ".\\build\\cibuild.cmd -configuration $config"
-            }
-            else {
-                buildCommand = "./build.sh --configuration $config"
-            }
-
-            def newJob = job(Utilities.getFullJobName(project, jobName, isPR)) {
-                // Set the label.
-                steps {
-                    if (osBase == 'Windows_NT') {
-                        // Batch
-                        batchFile(buildCommand)
-                    }
-                    else {
-                        // Shell
-                        shell(buildCommand)
+                def newJob = job(Utilities.getFullJobName(project, jobName, isPR)) {
+                    // Set the label.
+                    steps {
+                        if (osBase == 'Windows_NT') {
+                            // Batch
+                            batchFile(".\\build\\cibuild.cmd -configuration $config")
+                        }
+                        else {
+                            // Shell
+                            shell("./build.sh --configuration $config")
+                        }
                     }
                 }
-            }
 
-            def archiveSettings = new ArchivalSettings()
-            archiveSettings.addFiles("artifacts/$config/log/*")
-            archiveSettings.addFiles("artifacts/$config/TestResults/*")
-            archiveSettings.setFailIfNothingArchived()
-            archiveSettings.setArchiveOnFailure()
-            Utilities.setMachineAffinity(newJob, osBase, machineAffinity)
-            Utilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
-            Utilities.addGithubPRTriggerForBranch(newJob, branch, "$os $architecture $config")
-            Utilities.addArchival(newJob, archiveSettings)
+                def archiveSettings = new ArchivalSettings()
+                archiveSettings.addFiles("artifacts/$config/log/*")
+                archiveSettings.addFiles("artifacts/$config/TestResults/*")
+                archiveSettings.setFailIfNothingArchived()
+                archiveSettings.setArchiveOnFailure()
+                Utilities.setMachineAffinity(newJob, osBase, machineAffinity)
+                Utilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
+                Utilities.addGithubPRTriggerForBranch(newJob, branch, "$os $architecture $config")
+                Utilities.addArchival(newJob, archiveSettings)
         }
     }
 }
